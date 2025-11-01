0d670a46d75f6a05eb01a025077f113d0d1c64a0
Lucas Octave SOUBEIGA <lucasoctave3@gmail.com>
2025-11-01 13:03:39 +0100

    FIX: Ajout du LanguageProvider dans App.tsx.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,10 +3,13 @@
 import * as React from 'react';
 import { QueryClientProvider } from '@tanstack/react-query';
 
+// Importation du LanguageProvider nécessaire pour corriger l'erreur de la console
+import { LanguageProvider } from './contexts/LanguageContext'; // <-- AJOUTER CETTE LIGNE
+
 // Importation de la page d'accueil principale
 import Home from './pages/Home';
 
-// Importation du client de requête (le "problème" était souvent ici)
+// Importation du client de requête 
 import { queryClient } from './lib/queryClient'; 
 
 // Importation du composant Toaster pour les notifications
@@ -14,16 +17,17 @@ import { Toaster } from './components/ui/toaster';
 
 const App = () => {
   return (
+    // Nous enveloppons l'intégralité de l'application avec les deux fournisseurs
     <QueryClientProvider client={queryClient}>
-      
-      {/* AFFICHAGE DU COMPOSANT HOME - Votre page d'accueil */}
-      <Home />
-      
-      <Toaster />
+      <LanguageProvider>  {/* <-- ENVELOPPER ICI */}
+        
+        {/* AFFICHAGE DU COMPOSANT HOME - Votre page d'accueil */}
+        <Home />
+        
+        <Toaster />
+      </LanguageProvider> {/* <-- ET FERMER ICI */}
     </QueryClientProvider>
   );
 };
 
 export default App;
-
-
